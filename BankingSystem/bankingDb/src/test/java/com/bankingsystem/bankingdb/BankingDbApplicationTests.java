0982c984db50d3072e188552106bbd1ec743cb90
홍교 <111673196+LeeHongyo@users.noreply.github.com>
2024-08-01 22:04:20 +0900

    faet: setting multi module

---
+++ BankingSystem/bankingDb/src/test/java/com/bankingsystem/bankingdb/BankingDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankingsystem.bankingdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingDbApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
